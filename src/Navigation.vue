8eba8e618c26d7efd4bc6b59360f7160ba8f2901
Patricia <ps101@hdm-stuttgart>
2017-09-09 18:10:35 +0200

    Nav basic css

--- src/Navigation.vue
+++ src/Navigation.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
+    <header>
+      <img src="./assets/klaus_logo.png" id="logo"></img>
+    </header>
     <b-nav>
-      <b-nav-item active>Active</b-nav-item>
-      <b-nav-item>Link</b-nav-item>
-      <b-nav-item>Another Link</b-nav-item>
-      <b-nav-item disabled>Disabled</b-nav-item>
+      <b-nav-item active>fooo</b-nav-item>
+      <b-nav-item>Klausurensuche</b-nav-item>
+      <b-nav-item>Upload</b-nav-item>
     </b-nav>
   </div>
 </template>
@@ -12,5 +14,53 @@
 <script>
 </script>
 
-<style>
+<style scoped>
+  .nav{
+    padding: 0 15% 0 15%;
+    text-align: right;
+    justify-content: flex-end;
+  }
+
+  .nav-item{
+    padding: 0 0 32px 0;
+    margin: 0;
+  }
+
+header{
+  width: 100%;
+  padding: 0 15% 0 15%
+}
+
+  #logo{
+    height: 145px;
+  }
+
+  .nav-item:active {
+	position: relative;
+	background: #88b7d5;
+	border: 4px solid #c2e1f5;
+}
+.arrow_box:after, .arrow_box:before {
+	top: 100%;
+	left: 50%;
+	border: solid transparent;
+	content: " ";
+	height: 0;
+	width: 0;
+	position: absolute;
+	pointer-events: none;
+}
+
+.arrow_box:after {
+	border-color: rgba(136, 183, 213, 0);
+	border-top-color: #88b7d5;
+	border-width: 30px;
+	margin-left: -30px;
+}
+.arrow_box:before {
+	border-color: rgba(194, 225, 245, 0);
+	border-top-color: #c2e1f5;
+	border-width: 36px;
+	margin-left: -36px;
+}
 </style>
